86d23c358264dbd7f433af085fa42b6454cf9c98
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-11-12 14:33:45 +0300

    SearchBar update

--- flatiron/src/App.js
+++ flatiron/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
